beb264ca1223656a26b2603fa552bd52cbc841be
JolyMolyf <vs2001dor@gmail.com>
2023-07-09 16:51:08 +0200

    Added custom controller for graduates creation

--- src/api/webinar-participants-list/controllers/webinar-participants-list.js
+++ src/api/webinar-participants-list/controllers/webinar-participants-list.js
@@ -6,4 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::webinar-participants-list.webinar-participants-list');
+module.exports = createCoreController('api::webinar-participants-list.webinar-participants-list', ({strapi}) => ({
+    async create(ctx) {
+        const body = ctx.request.body.data
+        const participants = body.participants;
+        const graduates = [];
+       
+        for ( const participant of participants ) {
+            const results  = await strapi.service('api::graduate.graduate').create({ data: {
+                Name: participant.name,
+                Surname: participant.surname,
+                email: participant.email,
+                status: participant.status,
+                publishedAt: new Date()
+            }}).catch((e) => {
+                console.log('Erreor happende: ', e)
+            });
+            graduates.push(results);
+        }
+               
+        ctx.request.body.data = {...body, graduates: [...graduates]}
+        const { data, meta } = await super.create(ctx);
+        return { data, meta } 
+    }
+}));
