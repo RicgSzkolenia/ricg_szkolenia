784ef1a0752eef3316535eedc8521b0650b5f313
JolyMolyf <vs2001dor@gmail.com>
2023-09-10 17:33:18 +0200

    release fix

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -38,8 +38,8 @@ module.exports = createCoreController('api::order.order', ({strapi})=> ({
         try {
             const session = await stripe.checkout.sessions.create({
                 mode: "payment",
-                success_url: `http://localhost:3000?success=true`,
-                cancel_url: `http://localhost:3000?success=false`,
+                success_url: process.env.CLIENT_FRONT_URL || '',
+                cancel_url: process.env.CLIENT_FRONT_URL || '',
                 line_items: lineItems,
                 payment_method_types: ["card", "blik", "p24"],
                 allow_promotion_codes: true,
